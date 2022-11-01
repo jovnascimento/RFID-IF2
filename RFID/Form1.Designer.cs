3ac16b8d614c4d56f48fc894d9f4ae553bf53298
jovnascimento <jovnascimento08@hotmail.com>
2022-11-01 20:17:26 -0300

    Comentários

--- RFID/Form1.Designer.cs
+++ RFID/Form1.Designer.cs
@@ -47,6 +47,7 @@ namespace RFID
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -64,7 +65,7 @@ namespace RFID
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(492, 26);
+            this.listBox1.Location = new System.Drawing.Point(488, 26);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(307, 381);
             this.listBox1.TabIndex = 1;
@@ -72,7 +73,7 @@ namespace RFID
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(366, 26);
+            this.button2.Location = new System.Drawing.Point(362, 26);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 52);
             this.button2.TabIndex = 2;
@@ -179,7 +180,7 @@ namespace RFID
             // 
             this.textBox4.Location = new System.Drawing.Point(549, 449);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(108, 20);
             this.textBox4.TabIndex = 6;
             this.textBox4.Text = "TCP://192.168.0.198";
             // 
@@ -187,29 +188,29 @@ namespace RFID
             // 
             this.textBox3.Location = new System.Drawing.Point(549, 411);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(108, 20);
             this.textBox3.TabIndex = 7;
             this.textBox3.Text = "SERIAL://COM4";
             // 
             // radioButton4
             // 
             this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
             this.radioButton4.Location = new System.Drawing.Point(492, 452);
             this.radioButton4.Name = "radioButton4";
             this.radioButton4.Size = new System.Drawing.Size(61, 17);
             this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
             this.radioButton4.Text = "TCP/IP";
             this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Checked = true;
             this.radioButton3.Location = new System.Drawing.Point(492, 414);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(51, 17);
             this.radioButton3.TabIndex = 9;
-            this.radioButton3.TabStop = true;
             this.radioButton3.Text = "Serial";
             this.radioButton3.UseVisualStyleBackColor = true;
             // 
@@ -217,11 +218,19 @@ namespace RFID
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 482);
+            this.ClientSize = new System.Drawing.Size(804, 482);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton4);
             this.Controls.Add(this.textBox3);
@@ -259,6 +268,7 @@ namespace RFID
         private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- RFID/Form1.cs
+++ RFID/Form1.cs
@@ -24,10 +24,12 @@ namespace RFID
 
         private bool OpenReaderConnection()
         {
+            //inicializa variáveis necessárias
             bool status = false;
             string msg = null;
             string connect = null;
 
+            //verifica qual forma de conexão será usada
             if (radioButton3.Checked == true)
             {
                 //serial
@@ -35,14 +37,14 @@ namespace RFID
             }
             else
             {
-                //tcpip
+                //tcp/ip
                 connect = textBox4.Text;
             }
 
             try
             {
+                //tenta criar uma nova conexão, passando o endereço
                 brdr = new BRIReader(this, connect);
-
                 status = true;
             }
             catch (BasicReaderException ex)
@@ -50,34 +52,38 @@ namespace RFID
                 MessageBox.Show(ex.ToString());
                 status = false;
             }
-
+            //se não foi possível criar uma conexão e o status permaneceu falso
+            //a conexão falhou
             if (brdr == null || status == false)
             {
-                //failed to create reader connection
                 bReaderOffline = true;
                 PostMessageToListBox1("Não foi possível conectar ao receptor");
-                PostMessageToListBox1(connect);
+                PostMessageToListBox1("Endereço: " + connect);
                 return false;
             }
 
-            this.brdr.SetAutoPollTriggerEvents(false);
+            //this.brdr.SetAutoPollTriggerEvents(false);
 
+            //ao inicializar a conexão, primeiro executa um ping no receptor
             msg = this.brdr.Execute("PING");
             if (msg != null)
             {
+                //se a mensagem recebida for a padrão
                 if (msg.Equals("OK>"))
                 {
+                    //executa a verificação de versão e imprime a mensagem na caixa de mensagem
                     msg = this.brdr.Execute("VER");
                     ParseResponse(msg);
 
+                    //então, define que o receptor está online
                     bReaderOffline = false;
                     status = true;
                 }
             }
 
+            //se o status for falso após todo o processo, não foi possível conectar ao receptor
             if (status == false)
             {
-                //not connected to reader
                 PostMessageToListBox1("Não foi possível conectar ao receptor");
                 PostMessageToListBox1(connect);
                 bReaderOffline = true;
@@ -86,6 +92,7 @@ namespace RFID
             return status;
         }
 
+        //método tratar a mensagem enviada pelo IF2 e imprimí-la na caixa de mensagem
         private void ParseResponse(string msg)
         {
             int x = 0;
@@ -104,6 +111,7 @@ namespace RFID
             }
         }
 
+        //terminar a conexão com o IF2
         private void CloseReaderConnection()
         {
             if (brdr != null)
@@ -112,105 +120,110 @@ namespace RFID
                 brdr = null;
             }
         }
+
+        //método para uma leitura simples das tags
+        //ao abertar o botão de "Leitura simples", será feita a leitura de todas as tags próximas
+        //instantaneamente, sem uma poll ou leitura contínua
         private void ReadTags()
         {
-            //Simple read
-
             bool status = false;
 
+            //verificação se o receptor está online
             if (bReaderOffline == true)
             {
-                PostMessageToListBox1("Reader is offline");
+                PostMessageToListBox1("Receptor está offline");
                 return;
             }
 
             tagCount = 0;
 
+            //chama a função de leitura na antena
             status = brdr.Read();
 
-            //get the tag ids
+            //chama o método para pegar os IDs das tags
             LoadTagList();
         }
+
+        //método que busca os IDs definidos nas tags e imprime na caixa de mensagem
         private void LoadTagList()
         {
             if (brdr.TagCount > 0)
             {
+                //para cada tag identificada
                 foreach (Tag tt in brdr.Tags)
                 {
+                    //lista recebe as tags transformadas em string
                     MyTagList[++tagCount] = tt.ToString();
 
+                    //se as tags possuirem alguma informação
                     if (tt.TagFields.ItemCount > 0)
                     {
+                        //para cada tag, busca o TagField, que é o ID
                         foreach (TagField tf in tt.TagFields.FieldArray)
                         {
                             MyTagList[tagCount] += " " + tf.ToString();
                         }
                     }
+                    //imprime as tags na caixa de mensagem
                     PostMessageToListBox1(MyTagList[tagCount]);
                 }
             }
+            //caso não tenha/seja possível identificar nenhuma tag, imprime "NO TAGS"
             else
             {
                 PostMessageToListBox1("NO TAGS");
             }
         }
+        
+        //leitura contínua das tags definido por um determinado período de tempo em ms
+        //ao final da leitura, imprime todas as tags na caixa de mensagem
         private void ReadTagsReportNo()
         {
-            //Continuous read using polling to retrieve tag list
-
             bool status = false;
 
+            //verificação se o receptor está online
             if (bReaderOffline == true)
             {
-                PostMessageToListBox1("Reader is offline");
+                PostMessageToListBox1("Receptor está offline");
                 return;
             }
 
             tagCount = 0;
 
-            //determine how much time you want before polling for tag list.
-            //here it is set to 1 second
-            timer1.Interval = 5000;
+            //determina o período de tempo em ms para a leitura
+            //1000 ms = 1 segundo
+            timer1.Interval = 10000;
 
-            //Here are various read options
-            //Pick one and comment out the other two
-
-            //1. Read epc id's
+            //StartReadingTags recebe que a leitura será feita por poll
             status = brdr.StartReadingTags(null, null, BRIReader.TagReportOptions.POLL);
 
-            //2. Use a filter to read only tags who's epc id starts with hex 0102
-            //bStatus = brdr.StartReadingTags("HEX(1:4,2)=H0102", null, BRIReader.TagReportOptions.POLL);
-
-            //3. Return the antenna that read the tag and the number of times each tag was read
-            //bStatus = brdr.StartReadingTags(null, "ANT COUNT", BRIReader.TagReportOptions.POLL);
-
-            //enable timer to poll for tags
+            //inicializa o timer
             timer1.Enabled = true;
         }
+
+        //método que define como o timer deve agir após ser inicializado
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //enabled by ReadTagsReportNo() function
-            //timer used to poll for tags
+            bool status = false;
 
-            bool bStatus = false;
-
-            if (bReaderOffline == true) { return; }
+            //verificação se o receptor está online
+            //não é necessário imprimir nada pois esse aviso só é necessário quando as função são ativadas
+            if (bReaderOffline == true)
+            { 
+                return; 
+            }
 
             timer1.Enabled = false;
 
-            bStatus = brdr.PollTags();
+            status = brdr.PollTags();
 
-            //get the tag ids
             LoadTagList();
 
-            //you must issues a READ STOP to turn off the RF when you are done reading.
-            //normally you would not stop at this point but would continue to polling for tags
-            //but for this sample we are going to stop reading tags here.
+            //após o período determinado, a leitura é interrompida
             brdr.StopReadingTags();
-
-            //normally you would re-enable timer and continue polling for tags
-            //timer1.Enabled = true;
         }
+
+        //método para postar as mensagens na caixa de mensagem
         private void PostMessageToListBox1(string sMsg)
         {
             listBox1.Items.Add(sMsg);
