f0dd70204e9e44b6b910455d0515285bd2113dc9
jovnascimento <jovnascimento08@hotmail.com>
2022-11-01 19:38:05 -0300

    Commit inicial

---
+++ RFID/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace RFID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(492, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(307, 381);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(366, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ler";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 26);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(348, 381);
+            this.tabControl1.TabIndex = 3;
+            this.tabControl1.Tag = "";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(340, 355);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Propriedades";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(268, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(181, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Quantidade";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Volume";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ação";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(9, 70);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Remover";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 47);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Inserir";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(340, 355);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "Log";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(549, 449);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "TCP://192.168.0.198";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(549, 411);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "SERIAL://COM4";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(492, 452);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(61, 17);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.Text = "TCP/IP";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(492, 414);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 17);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Serial";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 482);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RFID/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Intermec.DataCollection.RFID;
+
+namespace RFID
+{
+    public partial class Form1 : Form
+    {
+        public BRIReader brdr = null;
+        private bool bReaderOffline = true;
+        private int tagCount = 0;
+        private string[] MyTagList = new string[100];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool OpenReaderConnection()
+        {
+            bool status = false;
+            string msg = null;
+            string connect = null;
+
+            if (radioButton3.Checked == true)
+            {
+                //serial
+                connect = textBox3.Text;
+            }
+            else
+            {
+                //tcpip
+                connect = textBox4.Text;
+            }
+
+            try
+            {
+                brdr = new BRIReader(this, connect);
+
+                status = true;
+            }
+            catch (BasicReaderException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                status = false;
+            }
+
+            if (brdr == null || status == false)
+            {
+                //failed to create reader connection
+                bReaderOffline = true;
+                PostMessageToListBox1("Não foi possível conectar ao receptor");
+                PostMessageToListBox1(connect);
+                return false;
+            }
+
+            this.brdr.SetAutoPollTriggerEvents(false);
+
+            msg = this.brdr.Execute("PING");
+            if (msg != null)
+            {
+                if (msg.Equals("OK>"))
+                {
+                    msg = this.brdr.Execute("VER");
+                    ParseResponse(msg);
+
+                    bReaderOffline = false;
+                    status = true;
+                }
+            }
+
+            if (status == false)
+            {
+                //not connected to reader
+                PostMessageToListBox1("Não foi possível conectar ao receptor");
+                PostMessageToListBox1(connect);
+                bReaderOffline = true;
+            }
+
+            return status;
+        }
+
+        private void ParseResponse(string msg)
+        {
+            int x = 0;
+            string delimStr = null;
+            string[] tList = null;
+            char[] delimiter = null;
+            int count = 0;
+
+            delimStr = "\n";
+            delimiter = delimStr.ToCharArray();
+            tList = msg.Replace("\r\n", "\n").Split(delimiter);
+            count = tList.Length;
+            for(x = 0; x < count; x++)
+            {
+                PostMessageToListBox1(tList[x]);
+            }
+        }
+
+        private void CloseReaderConnection()
+        {
+            if (brdr != null)
+            {
+                brdr.Dispose();
+                brdr = null;
+            }
+        }
+        private void ReadTags()
+        {
+            //Simple read
+
+            bool status = false;
+
+            if (bReaderOffline == true)
+            {
+                PostMessageToListBox1("Reader is offline");
+                return;
+            }
+
+            tagCount = 0;
+
+            status = brdr.Read();
+
+            //get the tag ids
+            LoadTagList();
+        }
+        private void LoadTagList()
+        {
+            if (brdr.TagCount > 0)
+            {
+                foreach (Tag tt in brdr.Tags)
+                {
+                    MyTagList[++tagCount] = tt.ToString();
+
+                    if (tt.TagFields.ItemCount > 0)
+                    {
+                        foreach (TagField tf in tt.TagFields.FieldArray)
+                        {
+                            MyTagList[tagCount] += " " + tf.ToString();
+                        }
+                    }
+                    PostMessageToListBox1(MyTagList[tagCount]);
+                }
+            }
+            else
+            {
+                PostMessageToListBox1("NO TAGS");
+            }
+        }
+        private void ReadTagsReportNo()
+        {
+            //Continuous read using polling to retrieve tag list
+
+            bool status = false;
+
+            if (bReaderOffline == true)
+            {
+                PostMessageToListBox1("Reader is offline");
+                return;
+            }
+
+            tagCount = 0;
+
+            //determine how much time you want before polling for tag list.
+            //here it is set to 1 second
+            timer1.Interval = 5000;
+
+            //Here are various read options
+            //Pick one and comment out the other two
+
+            //1. Read epc id's
+            status = brdr.StartReadingTags(null, null, BRIReader.TagReportOptions.POLL);
+
+            //2. Use a filter to read only tags who's epc id starts with hex 0102
+            //bStatus = brdr.StartReadingTags("HEX(1:4,2)=H0102", null, BRIReader.TagReportOptions.POLL);
+
+            //3. Return the antenna that read the tag and the number of times each tag was read
+            //bStatus = brdr.StartReadingTags(null, "ANT COUNT", BRIReader.TagReportOptions.POLL);
+
+            //enable timer to poll for tags
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //enabled by ReadTagsReportNo() function
+            //timer used to poll for tags
+
+            bool bStatus = false;
+
+            if (bReaderOffline == true) { return; }
+
+            timer1.Enabled = false;
+
+            bStatus = brdr.PollTags();
+
+            //get the tag ids
+            LoadTagList();
+
+            //you must issues a READ STOP to turn off the RF when you are done reading.
+            //normally you would not stop at this point but would continue to polling for tags
+            //but for this sample we are going to stop reading tags here.
+            brdr.StopReadingTags();
+
+            //normally you would re-enable timer and continue polling for tags
+            //timer1.Enabled = true;
+        }
+        private void PostMessageToListBox1(string sMsg)
+        {
+            listBox1.Items.Add(sMsg);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            listBox1.Refresh();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //botão para conectar com o IF2
+
+            bool status = true;
+            button1.Enabled = false;
+
+            status = OpenReaderConnection();
+
+            if (status == false)
+            {
+                //não foi possível conectar com o IF2
+                MessageBox.Show("Não foi possível conectar");
+                button1.Enabled = true;
+            }
+            else
+            {
+                //Precisa ser feito para a leitura contínua
+                //(AddRFIDEventHandlers, EventHandlerTag,
+                //ReadTagsReportEvent e timer1_Tick 
+                //AddRFIDEventHandlers();
+
+                PostMessageToListBox1("IF2 conectado!");
+
+                //enable READ button
+                button2.Enabled = true;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //botão para ler o RFID
+            if (bReaderOffline == true)
+            {
+                PostMessageToListBox1("Receptor está offline");
+                return;
+            }
+
+            //ReadTags();
+            ReadTagsReportNo();
+        }
+    }
+}
